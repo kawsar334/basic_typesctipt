ce63a09872a7f0a6237602121a4a6ece8b7c00b6
kawsar firoz <kawsarfiroz11@gmail.com>
2025-05-07 19:16:53 +0300

    added problem number 7

--- src/index.ts
+++ src/index.ts
@@ -117,4 +117,19 @@ const products = [
 console.log(getMostExpensiveProduct(products))
 
 // ============================Problem 7:========================================
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+function getDayType(day: Day): string{
+   return (day === Day.Saturday || day === Day.Sunday) ? "Weekend" : "Weekday"
+}
+console.log(getDayType(Day.Monday));   
+console.log(getDayType(Day.Sunday)); 
 // ============================Problem 8:========================================
