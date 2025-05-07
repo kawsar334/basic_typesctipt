6ca535e973c33d3dfacb31a4182290c39d9acb43
kawsar firoz <kawsarfiroz11@gmail.com>
2025-05-07 18:02:34 +0300

    solved problem number 2

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@
 // https://github.com/Apollo-Level2-Web-Dev/batch-4-be-a-typescript-technocrat/blob/main/practice_probelms.md
 
 
-
+// ============================Problem 1:========================================
 function formatString(input: string, toUpper?: boolean): string{
-        
     if(toUpper){
         return input.toUpperCase();
     }else{
@@ -13,8 +12,28 @@ function formatString(input: string, toUpper?: boolean): string{
 
 
 }
-
-
 console.log(formatString("Hello"));          // Output: "HELLO"
 console.log(formatString("Hello", true));   // Output: "HELLO"
 console.log(formatString("Hello", false));  // Output: "hello"
+
+// ============================Problem 2:========================================
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+    const filterArray= items.filter((item)=>item?.rating>=4)
+    return filterArray
+}
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+
+
+console.log(filterByRating(books))
+
+// ============================Problem 3:========================================
+// ============================Problem 4:========================================
+// ============================Problem 5:========================================
+// ============================Problem 6:========================================
+// ============================Problem 7:========================================
+// ============================Problem 8:========================================
